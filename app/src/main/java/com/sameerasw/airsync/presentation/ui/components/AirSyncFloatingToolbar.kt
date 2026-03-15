1cb46edad93b92d443747a6266a77692bbd03956
Corrado Belmonte <corradino.belmonte@gmail.com>
2026-03-16 00:26:15 +0100

    feat: Externalize expanded state in AirSyncFloatingToolbar

--- app/src/main/java/com/sameerasw/airsync/presentation/ui/components/AirSyncFloatingToolbar.kt
+++ app/src/main/java/com/sameerasw/airsync/presentation/ui/components/AirSyncFloatingToolbar.kt
@@ -25,9 +25,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -40,13 +37,11 @@ fun AirSyncFloatingToolbar(
     modifier: Modifier = Modifier,
     currentPage: Int,
     tabs: List<AirSyncTab>,
+    expanded: Boolean = true,
     onTabSelected: (Int) -> Unit,
     scrollBehavior: FloatingToolbarScrollBehavior? = null,
     floatingActionButton: @Composable () -> Unit = {}
 ) {
-    // Persistent visibility
-    var expanded by remember { mutableStateOf(true) }
-
     HorizontalFloatingToolbar(
 //        modifier = modifier
 //            .windowInsetsPadding(
